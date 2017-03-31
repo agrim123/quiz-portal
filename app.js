f86d812c00f90d9391831ff76722da3c943efaa2
hitman <agrimmittal97@gmail.com>
2017-04-01 00:46:25 +0530

    add emails for uncaughtException

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var nodemailer = require('nodemailer');
 var bodyParser = require('body-parser');
+
 var routes = require('./routes/routes');
 
 var app = express();
@@ -27,19 +29,46 @@ var pgp = require('pg-promise')
 , pgSession = require('connect-pg-simple')(session);
 
 app.use(session({
-	store: new pgSession({
+  store: new pgSession({
     conString : process.env.DATABASE_URL,
   }),
-	secret: process.env.COOKIE_SECRET,
-	resave: false,
+  secret: process.env.COOKIE_SECRET,
+  resave: false,
   saveUninitialized: false,
   cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 days
 }));
 
 app.use('/', routes);
 
+var transport = nodemailer.createTransport({ // [1]
+  service: "Gmail",
+  auth: {
+    user: "gmail.user@gmail.com",
+    pass: "userpass"
+  }
+});
+transport.sendMail({
+  from: 'alerts@mycompany.com',
+  to: 'alert@mycompany.com',
+  subject: 'asa',
+      text: 'as' // [4]
+    });
 
-/*app.use('/', routes);*/
+if (process.env.NODE_ENV === 'production') { // [2]
+  process.on('uncaughtException', function (er) {
+    console.error(er.stack) // [3]
+    transport.sendMail({
+      from: 'alerts@mycompany.com',
+      to: 'alert@mycompany.com',
+      subject: er.message,
+      text: er.stack // [4]
+    }, function (er) {
+     if (er) console.error(er)
+       process.exit(1) // [5]
+   })
+  })
+}
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
 	var err = new Error('Not Found');
